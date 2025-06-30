c488678e117ec2070113c28bfbeb2245834b262e
Kevin <fcclea@gmail.com>
2025-06-30 05:24:12 -0400

    update: API is working (few bugs)

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-const moviesWrapper = document.querySelector(".movie__result--container-1, .movie__result--container-2");
+const moviesWrapper = document.getElementById("movie__result--container");
 
 function searchChange(event) {
-    console.log(event.target.value);
+    renderMovies(event.target.value)
 }
 
-async function renderMovies() {
-    const response = await fetch('https://www.omdbapi.com/?s=Batman&apikey=1fb66a7f');
+async function renderMovies(searchTerm) {
+    const response = await fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=1fb66a7f`);
     const data = await response.json();
     const moviesArr = data.Search
-    moviesWrapper.innerHTML = moviesArr.map((movie) => {
+    moviesWrapper.innerHTML = moviesArr.slice(0,6).map((movie) => {
         return `
         <div class="movie__result">
-        <img class="movie__img" src="${movie.Poster}" alt="Hey">
+        <img class="movie__img" src="${movie.Poster}" alt="${movie.Title}">
         <h2 class="movie__title">${movie.Title}</h2>
         <h3 class="movie__year">${movie.Year}</h3>
         <button type="submit" class="learn-more_btn click">Learn More</button>
+        </div>
       `
-    })
+    }).join('');
 }
 
-renderMovies();
+
+
+
+
