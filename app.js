6fc761bee14a2085558c8e37e77c5f4670c51b00
Kevin <fcclea@gmail.com>
2025-06-29 21:07:57 -0400

    update

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-const movieListElem = document.getElementById("movie__result")
+const moviesWrapper = document.querySelector(".movie__result--container-1, .movie__result--container-2");
 
-async function main() {
-    const getInfo = await fetch('http://www.omdbapi.com/?t=Spider-Man&apikey=1fb66a7f')
-    const myInfo = await getInfo.json();
-    console.log(myInfo);
+function searchChange(event) {
+    console.log(event.target.value);
 }
 
-main();
-
-function search() {
-    
+async function renderMovies() {
+    const response = await fetch('https://www.omdbapi.com/?s=Batman&apikey=1fb66a7f');
+    const data = await response.json();
+    const moviesArr = data.Search
+    moviesWrapper.innerHTML = moviesArr.map((movie) => {
+        return `
+        <div class="movie__result">
+        <img class="movie__img" src="${movie.Poster}" alt="Hey">
+        <h2 class="movie__title">${movie.Title}</h2>
+        <h3 class="movie__year">${movie.Year}</h3>
+        <button type="submit" class="learn-more_btn click">Learn More</button>
+      `
+    })
 }
+
+renderMovies();
